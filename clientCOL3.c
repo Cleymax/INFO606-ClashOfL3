d001cd90458990a004be53efcc8fc5b9cc1ec5d6
Clement PERRIN <clement.perrin@etu.univ-smb.fr>
2023-03-06 16:23:22 +0100

    🚀  Add Step 2

--- clientCOL3.c
+++ clientCOL3.c
@@ -10,185 +10,308 @@
 #include "col3-bibtp/communCOL3-TP.h"
 #include "clientCOL3.h"
 
-
 extern clan_client MONCLAN;
 
 /* ===================================
-     fonction d'extraction des sites 
-       (a completer)
+	 fonction d'extraction des sites
+	   (a completer)
   ==================================*/
-		   
+
 void recupSiteExtraction()
 {
 	int socket;
 
-	logClientCOL3(info,"testConnexionServeur", 
-		"le clan[%s] crée une socket pour tester le serveur",
-		MONCLAN.nomDuClan);
-	
-	socket = connexionServeurCOL3(MONCLAN.adresseSrvCol3,MONCLAN.portTP1,MONCLAN.monToken,MONCLAN.nomDuClan);
+	logClientCOL3(info, "testConnexionServeur",
+				  "le clan[%s] crée une socket pour tester le serveur",
+				  MONCLAN.nomDuClan);
+
+	socket = connexionServeurCOL3(MONCLAN.adresseSrvCol3, MONCLAN.portTP1, MONCLAN.monToken, MONCLAN.nomDuClan);
 
 	/* si la socket est valide*/
-	if (socket != INVALID_SOCKET) {
-		logClientCOL3(info,"testConnexionServeur", 
-			"le clan[%s] a validé son test de connexion  %b ",
-			MONCLAN.nomDuClan,debug_ok); 
-	} 	else {
-		logClientCOL3(error,"testConnexionServeur", 
-			"le clan[%s] n'a pas validé son test de connexion  %b ",
-			MONCLAN.nomDuClan,debug_nok); 
+	if (socket != INVALID_SOCKET)
+	{
+		logClientCOL3(info, "testConnexionServeur",
+					  "le clan[%s] a validé son test de connexion  %b ",
+					  MONCLAN.nomDuClan, debug_ok);
+	}
+	else
+	{
+		logClientCOL3(error, "testConnexionServeur",
+					  "le clan[%s] n'a pas validé son test de connexion  %b ",
+					  MONCLAN.nomDuClan, debug_nok);
 	}
 
-	//concatenate MSG_SITE + MSG_DELIMITER  + MSG_QUEST
+	// concatenate MSG_SITE + MSG_DELIMITER  + MSG_QUEST
 
-	char* msg = malloc(strlen(MSG_SITE) + strlen(MSG_DELIMITER) + strlen(MSG_QUEST) + 1);
+	char *msg = malloc(strlen(MSG_SITE) + strlen(MSG_DELIMITER) + strlen(MSG_QUEST) + 1);
 	strcpy(msg, MSG_SITE);
 	strcat(msg, MSG_DELIMITER);
 	strcat(msg, MSG_QUEST);
 
-	//send message
-	if(envoiMessageCOL3_s(socket, msg) == -1) {
-		logClientCOL3(error,"testConnexionServeur", 
-			"le clan[%s] n'a pas pu envoyer %b ",
-			MONCLAN.nomDuClan,debug_nok);
-	}else {
-		logClientCOL3(info,"testConnexionServeur", 
-			"le clan[%s] a envoyé %b ",
-			MONCLAN.nomDuClan,debug_ok);
+	// send message
+	if (envoiMessageCOL3_s(socket, msg) == -1)
+	{
+		logClientCOL3(error, "testConnexionServeur",
+					  "le clan[%s] n'a pas pu envoyer %b ",
+					  MONCLAN.nomDuClan, debug_nok);
+	}
+	else
+	{
+		logClientCOL3(info, "testConnexionServeur",
+					  "le clan[%s] a envoyé %b ",
+					  MONCLAN.nomDuClan, debug_ok);
 	}
 
-	//receive message
-	if(lireStructureCOL3_s(socket, &MONCLAN.mesCapacites, sizeof(MONCLAN.mesCapacites))== -1){
-		logClientCOL3(error,"testConnexionServeur", 
-			"le clan[%s] n'a pas pu lire %b ",
-			MONCLAN.nomDuClan,debug_nok);
-	}else {
-		logClientCOL3(info,"testConnexionServeur", 
-			"le clan[%s] a lu %b ",
-			MONCLAN.nomDuClan,debug_ok);
+	// receive message
+	if (lireStructureCOL3_s(socket, &MONCLAN.mesCapacites, sizeof(MONCLAN.mesCapacites)) == -1)
+	{
+		logClientCOL3(error, "testConnexionServeur",
+					  "le clan[%s] n'a pas pu lire %b ",
+					  MONCLAN.nomDuClan, debug_nok);
+	}
+	else
+	{
+		logClientCOL3(info, "testConnexionServeur",
+					  "le clan[%s] a lu %b ",
+					  MONCLAN.nomDuClan, debug_ok);
 	}
 
 	afficheCapaciteDuClan(MONCLAN.mesCapacites);
 	close(socket);
 }
 
-
 /* ===================================
-     fonction de recupération de MP 
-       (a completer)
+	 fonction de recupération de MP
+	   (a completer)
   ==================================*/
-		   
+
+// fonction gestionChariot
+
+void gestionChariot(void *arg)
+{
+	int socket = (int)arg;
+
+
+	// concat MSG_CHARIOT + MSG_DELIMITER + MSG_QUEST
+
+	char *msg = malloc(strlen(MSG_CHARIOT) + strlen(MSG_DELIMITER) + strlen(MSG_QUEST) + 1);
+	strcpy(msg, MSG_CHARIOT);
+	strcat(msg, MSG_DELIMITER);
+	strcat(msg, MSG_QUEST);
+
+	// send message
+	if (envoiMessageCOL3_s(socket, msg) == -1)
+	{
+		logClientCOL3(error, "gestionAppro",
+					  "le clan[%s] n'a pas pu envoyer %b ",
+					  MONCLAN.nomDuClan, debug_nok);
+	}
+	else
+	{
+		logClientCOL3(info, "gestionAppro",
+					  "le clan[%s] a envoyé %b ",
+					  MONCLAN.nomDuClan, debug_ok);
+	}
+
+	char msgrecu[TAILLE_MAX_MSG];
+
+	lireMessageCOL3_s(socket, msgrecu);
+
+	logClientCOL3(info, "gestionAppro",
+				  "le clan[%s] a reçu %s %b ",
+				  MONCLAN.nomDuClan,&msgrecu, debug_ok);
+
+	if(strcmp(msgrecu, MSG_CHARIOT_OK)){
+		logClientCOL3(info, "gestionAppro",
+					  "le clan[%s] a reçu MSG_CHARIOT_OK %b ",
+					  MONCLAN.nomDuClan, debug_ok);
+
+
+		char *msg = malloc(strlen(MSG_CHARIOT) + strlen(MSG_DELIMITER) + sizeof(int) + 1);
+		strcpy(msg, MSG_CHARIOT);
+		strcat(msg, MSG_DELIMITER);
+		strcat(msg, MONCLAN.mesCapacites.sitesAccessibles->idSite);
+
+		if(envoiMessageCOL3_s(socket, msg)== -1){
+				logClientCOL3(error, "gestionAppro",
+					  "le clan[%s] n'a pas pu envoyer %b ",
+					  MONCLAN.nomDuClan, debug_nok);
+		}else {
+
+			char msgrecu2[TAILLE_MAX_MSG];
+
+			lireMessageCOL3_s(socket, msgrecu2);
+
+			logClientCOL3(info, "gestionAppro",
+						"le clan[%s] a reçu %s %b ",
+						MONCLAN.nomDuClan,&msgrecu2, debug_ok);
+
+			if(strcmp(msgrecu2, MSG_STOP)){
+				logClientCOL3(error, "gestionAppro",
+					  "le clan[%s] a reçu MSG_STOP %b ",
+					  MONCLAN.nomDuClan, debug_nok);
+			}else {
+				
+			}
+					
+		}
+
+	}else {
+		logClientCOL3(error, "gestionAppro",
+					  "le clan[%s] a recu MSG_STOP %b ",
+					  MONCLAN.nomDuClan, debug_nok);
+	}
+}
+
 void gestionAppro()
 {
 
- printf("\n  *** au boulot ... ****\n");
-	
+	printf("\n  *** au boulot ... ****\n");
+
+	// gérer les approvisionnements, c'est à dire que vous envoyez autant de chariots que vous avez le droit avec un thread par chariot
+
+	int socket;
+
+	logClientCOL3(info, "gestionAppro",
+				  "le clan[%s] crée une socket pour tester le serveur",
+				  MONCLAN.nomDuClan);
+
+	socket = connexionServeurCOL3(MONCLAN.adresseSrvCol3, MONCLAN.portTP1, MONCLAN.monToken, MONCLAN.nomDuClan);
+
+	/* si la socket est valide*/
+	if (socket != INVALID_SOCKET)
+	{
+		logClientCOL3(info, "gestionAppro",
+					  "le clan[%s] a validé son test de connexion  %b ",
+					  MONCLAN.nomDuClan, debug_ok);
+	}
+	else
+	{
+		logClientCOL3(error, "gestionAppro",
+					  "le clan[%s] n'a pas validé son test de connexion  %b ",
+					  MONCLAN.nomDuClan, debug_nok);
+	}
+
+	int i = 0;
+	pthread_t threadChariot[MONCLAN.mesCapacites.nbChariotDisponible];
+
+	for (i = 0; i < MONCLAN.mesCapacites.nbChariotDisponible; i++)
+	{
+		pthread_create(&threadChariot, NULL, gestionChariot, (void *)socket);
+	}
+	close(socket);
 }
 
-
 /*  ======================================
-	  fonction de test d'échange initiale 
-      avec le serveur
-    =====================================*/ 
+	  fonction de test d'échange initiale
+	  avec le serveur
+	=====================================*/
 
-int testConnexionServeur() {	
+int testConnexionServeur()
+{
 	int socket;
 	int testOK = 0;
 
-	logClientCOL3(info,"testConnexionServeur", 
-		"le clan[%s] crée une socket pour tester le serveur",
-		MONCLAN.nomDuClan);
-	
+	logClientCOL3(info, "testConnexionServeur",
+				  "le clan[%s] crée une socket pour tester le serveur",
+				  MONCLAN.nomDuClan);
+
 	/* creation et connexion au serveur de socket ( on met MSG_TEST à la place du TOKEN ) */
-	socket = connexionServeurCOL3(MONCLAN.adresseSrvCol3,MONCLAN.portTP1,MSG_TEST,MONCLAN.nomDuClan);
+	socket = connexionServeurCOL3(MONCLAN.adresseSrvCol3, MONCLAN.portTP1, MSG_TEST, MONCLAN.nomDuClan);
 
 	/* si la socket est valide*/
-	if (socket != INVALID_SOCKET) {
-		logClientCOL3(info,"testConnexionServeur", 
-			"le clan[%s] a validé son test de connexion  %b ",
-			MONCLAN.nomDuClan,debug_ok); 
+	if (socket != INVALID_SOCKET)
+	{
+		logClientCOL3(info, "testConnexionServeur",
+					  "le clan[%s] a validé son test de connexion  %b ",
+					  MONCLAN.nomDuClan, debug_ok);
 		close(socket);
-		testOK=1;
-	} 	else {
-		logClientCOL3(error,"testConnexionServeur", 
-			"le clan[%s] n'a pas validé son test de connexion  %b ",
-			MONCLAN.nomDuClan,debug_nok); 
+		testOK = 1;
+	}
+	else
+	{
+		logClientCOL3(error, "testConnexionServeur",
+					  "le clan[%s] n'a pas validé son test de connexion  %b ",
+					  MONCLAN.nomDuClan, debug_nok);
 	}
 	return testOK;
 }
-	
+
 void afficheMesInfos()
 {
-  logClientCOL3(info,"afficheMesInfos", 
-		"Nom du Clan   = %s",
-		MONCLAN.nomDuClan);
-  logClientCOL3(info,"afficheMesInfos", 
-		"Token du Clan = %s",
-		MONCLAN.monToken);
-  logClientCOL3(info,"afficheMesInfos", 
-		"Serveur COL3  = %s | port TP1 = %d | port TP2 = %d | port TP3 = %d",
-		MONCLAN.adresseSrvCol3,MONCLAN.portTP1,MONCLAN.portTP2,MONCLAN.portTP3);
+	logClientCOL3(info, "afficheMesInfos",
+				  "Nom du Clan   = %s",
+				  MONCLAN.nomDuClan);
+	logClientCOL3(info, "afficheMesInfos",
+				  "Token du Clan = %s",
+				  MONCLAN.monToken);
+	logClientCOL3(info, "afficheMesInfos",
+				  "Serveur COL3  = %s | port TP1 = %d | port TP2 = %d | port TP3 = %d",
+				  MONCLAN.adresseSrvCol3, MONCLAN.portTP1, MONCLAN.portTP2, MONCLAN.portTP3);
 }
 
 /**
  * fonction qui calcul si le menu est valide
- * 
+ *
  */
 int estMenuValide(char menu[])
 {
-  menu[1]='\0'; /* on veut juste le premier caractere */
-  int ret = (strcmp(menu,"1")==0) || (strcmp(menu,"2")==0) ||
-				(strcmp(menu,"3")==0) || (strcmp(menu,"4")==0) ||
-				(strcmp(menu,"5")==0) ||
-				(strcmp(menu,"q")==0) ;
-  return ret;
+	menu[1] = '\0'; /* on veut juste le premier caractere */
+	int ret = (strcmp(menu, "1") == 0) || (strcmp(menu, "2") == 0) ||
+			  (strcmp(menu, "3") == 0) || (strcmp(menu, "4") == 0) ||
+			  (strcmp(menu, "5") == 0) ||
+			  (strcmp(menu, "q") == 0);
+	return ret;
 }
 
 /**
  * procedure affichant le menu
- * 
+ *
  */
-void afficheMenuClient() {
+void afficheMenuClient()
+{
 	char clavier[10];
 
-	do {
-		do {
+	do
+	{
+		do
+		{
 			printf("\n\n============= menu COL3 ====================\n");
 			printf("1 - Afficher les informations de mon clan\n");
 			printf("2 - Tester la communication avec le serveur\n");
 			printf("3 - Récupérer la capacité d'extraction de mon clan\n");
 			printf("4 - Envoyer mes chariots et remplir ma hutte\n");
- 			printf("5 - Afficher ma hutte\n");
+			printf("5 - Afficher ma hutte\n");
 			printf("Q - quitter\n\n");
 			printf("Choix ? ");
-			fgets(clavier, 10, stdin);	
+			fgets(clavier, 10, stdin);
 			printf("============================================\n\n");
 		} while (!estMenuValide(clavier));
 
-	if (strcmp(clavier,"1")==0) afficheMesInfos();
-	else if (strcmp(clavier,"2")==0) 
-	  testConnexionServeur();
-	else if (strcmp(clavier,"3")==0) 
-	  recupSiteExtraction (MONCLAN.adresseSrvCol3,
-	                       MONCLAN.portTP1,
-	                       MONCLAN.monToken,
-	                       MONCLAN.nomDuClan);
-	else if (strcmp(clavier,"4")==0) 
-	  {
-	   recupSiteExtraction (MONCLAN.adresseSrvCol3,
-	                       MONCLAN.portTP1,
-	                       MONCLAN.monToken,
-	                       MONCLAN.nomDuClan);
-	   gestionAppro(MONCLAN.adresseSrvCol3,
-	                MONCLAN.portTP1,
-	                MONCLAN.monToken,
-	                MONCLAN.nomDuClan,5);
-	}
-	else if (strcmp(clavier,"5")==0) 
-	   afficheHutte(MONCLAN.maHutte);
+		if (strcmp(clavier, "1") == 0)
+			afficheMesInfos();
+		else if (strcmp(clavier, "2") == 0)
+			testConnexionServeur();
+		else if (strcmp(clavier, "3") == 0)
+			recupSiteExtraction(MONCLAN.adresseSrvCol3,
+								MONCLAN.portTP1,
+								MONCLAN.monToken,
+								MONCLAN.nomDuClan);
+		else if (strcmp(clavier, "4") == 0)
+		{
+			recupSiteExtraction(MONCLAN.adresseSrvCol3,
+								MONCLAN.portTP1,
+								MONCLAN.monToken,
+								MONCLAN.nomDuClan);
+			gestionAppro(MONCLAN.adresseSrvCol3,
+						 MONCLAN.portTP1,
+						 MONCLAN.monToken,
+						 MONCLAN.nomDuClan, 5);
+		}
+		else if (strcmp(clavier, "5") == 0)
+			afficheHutte(MONCLAN.maHutte);
 
-	} while (!(strcmp(clavier,"q")==0));
+	} while (!(strcmp(clavier, "q") == 0));
 
-    printf("Bye-bye %s !\n\n",MONCLAN.nomDuClan);
-
+	printf("Bye-bye %s !\n\n", MONCLAN.nomDuClan);
 }
